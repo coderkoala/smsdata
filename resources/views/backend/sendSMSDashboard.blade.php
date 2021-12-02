3e64381de656a474b20a68d096f906ea3e0b9c27
iamtribulation@outlook.com <iamtribulation@outlook.com>
2021-12-02 09:15:36 +0545

    Adding metrics in dashboard

--- resources/views/backend/sendSMSDashboard.blade.php
+++ resources/views/backend/sendSMSDashboard.blade.php
@@ -3,11 +3,69 @@
 @section('title', __('SMS Dashboard'))
 
 @section('content')
+<div class="row">
+    <div class="col-sm-6 col-lg-3">
+        <div class="card text-white bg-primary">
+            <div class="card-body card-body pb-0 d-flex justify-content-between align-items-start">
+                <div>
+                    <div class="text-value-lg">{{App\Models\models\SMSData::count()}}</div>
+                    <div>Total Messages</div>
+                </div>
+            </div>
+            <div class="c-chart-wrapper mt-3 mx-3" style="height: 70px">
+                <canvas class="chart" id="card-chart1" height="70"></canvas>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-6 col-lg-3">
+        <div class="card text-white bg-success">
+            <div class="card-body card-body pb-0 d-flex justify-content-between align-items-start">
+                <div>
+                    <div class="text-value-lg">{{App\Models\models\SMSData::where('isSent', 'Y')->count()}}</div>
+                    <div>Sent Messages</div>
+                </div>
+            </div>
+            <div class="c-chart-wrapper mt-3 mx-3" style="height: 70px">
+                <canvas class="chart" id="card-chart2" height="70"></canvas>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-6 col-lg-3">
+        <div class="card text-white bg-info">
+            <div class="card-body card-body pb-0 d-flex justify-content-between align-items-start">
+                <div>
+                    <div class="text-value-lg">{{App\Models\models\Contacts::where('IsLive', 'Y')->count()}}</div>
+                    <div>Total Contacts</div>
+                </div>
+            </div>
+            <div class="c-chart-wrapper mt-3" style="height: 70px">
+                <canvas class="chart" id="card-chart3" height="70"></canvas>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-6 col-lg-3">
+        <div class="card text-white bg-danger">
+            <div class="card-body card-body pb-0 d-flex justify-content-between align-items-start">
+                <div>
+                    <div class="text-value-lg">{{App\Models\models\SMSData::where('isSent', '!=' , 'Y')->orWhereNull('isSent')->count()}}</div>
+                    <div>Unsent Messages</div>
+                </div>
+            </div>
+            <div class="c-chart-wrapper mt-3 mx-3" style="height: 70px">
+                <canvas class="chart" id="card-chart4" height="70"></canvas>
+            </div>
+        </div>
+    </div>
+</div>
+
     <x-backend.card>
         <x-slot name="header">
             <h5 class="d-flex justify-content-between align-items-center">
                 SMS Management Service Dashboard
-                <a href="{{route('admin.new-sms')}}" type="button" class="btn btn-sm btn-primary">New SMS Dispatch</a>
+                <a href="{{ route('admin.new-sms') }}" type="button" class="btn btn-sm btn-primary">New SMS Dispatch</a>
             </h5>
         </x-slot>
 
@@ -26,7 +84,7 @@
                         <tr>
                             <td>{{ '' === $tuple['ID'] ? '-' : $tuple['ID'] }}</td>
                             <td>{{ null === $tuple['PhoneNo'] ? 'N/A' : $tuple['PhoneNo'] }}</td>
-                            <td>{{ '' === $tuple['SmsText'] ? '-' : substr($tuple['SmsText'], 0 , 30) . ' [...]' }}</td>
+                            <td>{{ '' === $tuple['SmsText'] ? '-' : substr($tuple['SmsText'], 0, 30) . ' [...]' }}</td>
                             <td>{{ null === $tuple['SentDate'] ? 'N/A' : $tuple['SentDate'] }}</td>
                             <td>{{ 'Y' === $tuple['isSent'] ? 'S' : 'U' }}</td>
                             <td>{{ '' === $tuple['CreatedBy'] ? '-' : $tuple['CreatedBy'] }}</td>
